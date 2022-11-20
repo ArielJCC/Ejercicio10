3d0b6e4ed1c640c251c4b5d4217c38c28f3e3c6a
unknown <xx06102002@gmail.com>
2022-11-20 16:57:00 -0500

    Primer Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Programa que lea un número entero por teclado y que calcule su factorial.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num; int factorial = 1; int i = 1;
+
+        Console.WriteLine("Digite un número");
+        num = int.Parse(Console.ReadLine());
+        while(i <= num)
+        {
+            factorial = factorial * i;
+             i++;
+
+        }
+        Console.WriteLine("El factorial de "+num+" es: "+factorial);
+        
+    }
+}
